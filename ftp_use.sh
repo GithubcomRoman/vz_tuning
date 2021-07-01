c3b98b8e4be07a5a2f32bacb2a78cbcbf5e2b7c6
GithubcomRoman <35697476+GithubcomRoman@users.noreply.github.com>
2021-07-01 15:48:23 +0200

    Create ftp_use.sh

---
+++ ftp_use.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+login=""
+password=""
+host=""
+
+log_mb=/var/log/backups/ftp_use.log
+
+
+size=$(lftp -e 'du -mc; bye;' -u $login,$password $host | grep total | awk '{print $1}')
+
+echo "backup size in mb: $size"
+echo $size > $log_mb
