2cb18b914a232e2112c07921fee1dc671ff4bd88
GithubcomRoman <35697476+GithubcomRoman@users.noreply.github.com>
2021-07-01 18:39:07 +0200

    Update ftp_use.sh

--- ftp_use.sh
+++ ftp_use.sh
@@ -9,4 +9,6 @@ log_mb=/var/log/backups/ftp_use.log
 size=$(lftp -e 'du -mc; bye;' -u $login,$password $host | grep total | awk '{print $1}')
 
 echo "backup size in mb: $size"
+
+
 echo $size > $log_mb
